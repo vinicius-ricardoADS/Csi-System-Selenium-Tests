25683b04a1207acf3c310bf4b967d1fa6b46fb76
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-05-25 10:44:55 -0300

    feat(src/test): simple test with selenium

---
+++ src/test/java/org/example/CsiTests.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class CsiTests {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    void setUp(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+    }
+    @AfterEach
+    void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    @DisplayName("Simple test")
+    void simpleTest () throws InterruptedException {
+        driver.get("http://localhost:3000/");
+        Thread.sleep(2000);
+    }
+}
